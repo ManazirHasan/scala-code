5d1661347fe87d60e8e4cc9d7a75abee73149fe9
Manazir <mkhanj2ee@gmail.com>
2023-02-27 00:04:55 +0530

    ADDED

--- src/main/scala/SparkSessionTest.scala
+++ src/main/scala/SparkSessionTest.scala
@@ -4,8 +4,8 @@ import org.apache.spark.sql.SparkSession
 class SparkSessionTest {
 
 	def sessionObject(): SparkSession = {
-		Logger.getLogger("org").setLevel(Level.OFF)
-		Logger.getLogger("akka").setLevel(Level.OFF)
+		//Logger.getLogger("org").setLevel(Level.OFF)
+		//Logger.getLogger("akka").setLevel(Level.OFF)
 		var ss = SparkSession.builder().master("local[2]").getOrCreate();
 		return ss;
 	}
